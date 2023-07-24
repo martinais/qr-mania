f6cfa7a4939f5a1ef028fef23b3a54bb483b5dce
Tristan Pinaudeau <tristan@tic.sh>
2023-07-25 00:29:44 +0200

    flask integration with google oauth

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import requests
+import os
+from google_auth_oauthlib.flow import Flow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-
-response = requests.get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json")
-
-
-print(response)
 
 app = Flask(__name__)
 
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/userinfo.profile']
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+flow = Flow.from_client_secrets_file(
+    'martinade.json',
+    scopes=SCOPES,
+    redirect_uri='http://localhost:5000/run'
+)
+
+
+@app.route('/run')
+def add_book():
+    flow.fetch_token(code=request.args.get('code'))
+    session = flow.authorized_session()
+    name = session.get(
+        'https://www.googleapis.com/userinfo/v2/me').json()['name']
+    return f"I'm {name} and I borrowed {request.args.get('state')}"
+
+
+@app.route('/loan/<int:book_id>')
+def loan(book_id):
+    return redirect(flow.authorization_url(state=book_id)[0])
 
 
 if __name__ == '__main__':
